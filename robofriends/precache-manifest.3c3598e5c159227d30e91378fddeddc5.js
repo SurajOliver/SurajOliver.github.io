87cfbcb9181fa7b1ab5a86aa453a1854fb449a76
Suraj Oliver <suraj@Surajs-MacBook-Air-2.local>
2019-02-17 12:29:46 +0530

    robofriends react app

---
+++ robofriends/precache-manifest.3c3598e5c159227d30e91378fddeddc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3ae84097036fa27ecc3",
+    "url": "/static/js/main.e3ae8409.chunk.js"
+  },
+  {
+    "revision": "6176eaec61bc224bcaa7",
+    "url": "/static/js/1.6176eaec.chunk.js"
+  },
+  {
+    "revision": "e3ae84097036fa27ecc3",
+    "url": "/static/css/main.067f8b71.chunk.css"
+  },
+  {
+    "revision": "6176eaec61bc224bcaa7",
+    "url": "/static/css/1.52a6232f.chunk.css"
+  },
+  {
+    "revision": "2703828ce3c8b5184375a6f9a2b0df7a",
+    "url": "/index.html"
+  }
+];
